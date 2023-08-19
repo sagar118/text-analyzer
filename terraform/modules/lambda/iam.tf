6641ee55c3815beda9630d6dbb0570f3f1e38087
EC2 Default User <ec2-user@ip-10-0-1-104.ec2.internal>
2023-08-19 14:54:20 +0000

    gave lambda cloudwatch log access

--- terraform/modules/lambda/iam.tf
+++ terraform/modules/lambda/iam.tf
@@ -14,3 +14,25 @@ resource "aws_iam_role" "lambda_exec" {
     ]
   })
 }
+
+resource "aws_iam_policy" "function_logging_policy" {
+  name   = "function-logging-policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
+  role = aws_iam_role.lambda_exec.id
+  policy_arn = aws_iam_policy.function_logging_policy.arn
+}
--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -12,3 +12,11 @@ resource "aws_lambda_function" "lambda" {
 
   timeout = 180
 }
+
+resource "aws_cloudwatch_log_group" "function_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
+}
