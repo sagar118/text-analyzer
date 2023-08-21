039d849287f7688d72485686db41236233f8788c
EC2 Default User <ec2-user@ip-10-0-1-104.ec2.internal>
2023-08-21 08:51:58 +0000

    Added monitoring and re-training files

---
+++ monitoring/evidently_grafana_metrics.py
@@ -0,0 +1,217 @@
+import math
+import time
+import logging
+import datetime
+from pprint import pprint
+
+import pandas as pd
+import psycopg
+from joblib import load
+from prefect import flow, task
+from evidently import ColumnMapping
+from evidently.report import Report
+from evidently.metric_preset import TextOverviewPreset, ClassificationPreset
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s"
+)
+SEND_TIMEOUT = 10
+begin = datetime.datetime(2023, 8, 1, 0, 0, tzinfo=datetime.timezone.utc)
+
+create_table_query = """
+create table if not exists metrics (
+    timestamp timestamp,
+    current_missing_count integer,
+    reference_missing_count integer,
+    current_text_length_mean float,
+    reference_text_length_mean float,
+    current_oov_mean float,
+    reference_oov_mean float,
+    current_non_letter_char_mean float,
+    reference_non_letter_char_mean float,
+    non_letter_char_drift_score float,
+    oov_drift_score float,
+    text_length_drift_score float,
+    current_accuracy_score float,
+    reference_accuracy_score float,
+    current_precision_score float,
+    reference_precision_score float,
+    current_recall_score float,
+    reference_recall_score float,
+    current_f1_score float,
+    reference_f1_score float
+);
+"""
+
+# Load data and model
+current_data = pd.read_parquet('./data/current.parquet')
+reference_data = pd.read_parquet('./data/reference.parquet')
+with open('models/log_reg.pkl', 'rb') as handle:
+    model = load(handle)
+
+col_mapping = ColumnMapping(
+    text_features=['processed_text'], target='target', prediction='prediction'
+)
+
+report = Report(
+    metrics=[TextOverviewPreset(column_name='processed_text'), ClassificationPreset()]
+)
+
+
+@task
+def prep_db():
+    with psycopg.connect(
+        "host=localhost port=5432 user=postgres password=postgres", autocommit=True
+    ) as conn:
+        # conn.autocommit = True
+        # curr = conn.cursor()
+        res = conn.execute("SELECT 1 FROM pg_database WHERE datname = 'evidently'")
+        if len(res.fetchall()) == 0:
+            conn.execute("CREATE DATABASE evidently;")
+        with psycopg.connect(
+            "host=localhost port=5432 user=postgres password=postgres dbname=evidently",
+            autocommit=True,
+        ) as conn:
+            # curr = conn.cursor()
+            conn.execute(create_table_query)
+
+
+@task
+def calculate_metrics_postgresql(curr, i):
+    current = current_data.iloc[i * 500 : (i + 1) * 500]
+    current['prediction'] = model.predict(current['processed_text'])
+
+    report.run(
+        current_data=current, reference_data=reference_data, column_mapping=col_mapping
+    )
+    json_data = report.as_dict()
+
+    current_missing_count = json_data['metrics'][0]['result'][
+        'current_characteristics'
+    ]['missing']
+    reference_missing_count = json_data['metrics'][0]['result'][
+        'reference_characteristics'
+    ]['missing']
+
+    current_text_length_mean = json_data['metrics'][0]['result'][
+        'current_characteristics'
+    ]['text_length_mean']
+    reference_text_length_mean = json_data['metrics'][0]['result'][
+        'reference_characteristics'
+    ]['text_length_mean']
+
+    current_oov_mean = json_data['metrics'][0]['result']['current_characteristics'][
+        'oov_mean'
+    ]
+    reference_oov_mean = json_data['metrics'][0]['result']['reference_characteristics'][
+        'oov_mean'
+    ]
+
+    current_non_letter_char_mean = json_data['metrics'][0]['result'][
+        'current_characteristics'
+    ]['non_letter_char_mean']
+    reference_non_letter_char_mean = json_data['metrics'][0]['result'][
+        'reference_characteristics'
+    ]['non_letter_char_mean']
+
+    non_letter_char_drift_score = json_data['metrics'][4]['result']['drift_by_columns'][
+        'Non Letter Character %'
+    ]['drift_score']
+    oov_drift_score = json_data['metrics'][4]['result']['drift_by_columns']['OOV %'][
+        'drift_score'
+    ]
+    text_length_drift_score = json_data['metrics'][4]['result']['drift_by_columns'][
+        'Text Length'
+    ]['drift_score']
+
+    current_accuracy_score = json_data['metrics'][5]['result']['current']['accuracy']
+    reference_accuracy_score = json_data['metrics'][5]['result']['reference'][
+        'accuracy'
+    ]
+
+    current_precision_score = json_data['metrics'][5]['result']['current']['precision']
+    reference_precision_score = json_data['metrics'][5]['result']['reference'][
+        'precision'
+    ]
+
+    current_recall_score = json_data['metrics'][5]['result']['current']['recall']
+    reference_recall_score = json_data['metrics'][5]['result']['reference']['recall']
+
+    current_f1_score = json_data['metrics'][5]['result']['current']['f1']
+    reference_f1_score = json_data['metrics'][5]['result']['reference']['f1']
+
+    curr.execute(
+        """
+        INSERT INTO metrics (
+            timestamp,
+            current_missing_count,
+            reference_missing_count,
+            current_text_length_mean,
+            reference_text_length_mean,
+            current_oov_mean,
+            reference_oov_mean,
+            current_non_letter_char_mean,
+            reference_non_letter_char_mean,
+            non_letter_char_drift_score,
+            oov_drift_score,
+            text_length_drift_score,
+            current_accuracy_score,
+            reference_accuracy_score,
+            current_precision_score,
+            reference_precision_score,
+            current_recall_score,
+            reference_recall_score,
+            current_f1_score,
+            reference_f1_score
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+        (
+            begin + datetime.timedelta(i),
+            current_missing_count,
+            reference_missing_count,
+            current_text_length_mean,
+            reference_text_length_mean,
+            current_oov_mean,
+            reference_oov_mean,
+            current_non_letter_char_mean,
+            reference_non_letter_char_mean,
+            non_letter_char_drift_score,
+            oov_drift_score,
+            text_length_drift_score,
+            current_accuracy_score,
+            reference_accuracy_score,
+            current_precision_score,
+            reference_precision_score,
+            current_recall_score,
+            reference_recall_score,
+            current_f1_score,
+            reference_f1_score,
+        ),
+    )
+
+
+@flow
+def batch_monitoring():
+    prep_db()
+    ROWS = current_data.shape[0]
+    iters = math.ceil(ROWS / 500)
+    last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
+    with psycopg.connect(
+        "host=localhost port=5432 dbname=evidently user=postgres password=postgres",
+        autocommit=True,
+    ) as conn:
+        # conn.autocommit = True
+        for i in range(iters):
+            with conn.cursor() as curr:
+                calculate_metrics_postgresql(curr, i)
+
+            new_send = datetime.datetime.now()
+            seconds_elapsed = (new_send - last_send).total_seconds()
+            if seconds_elapsed < SEND_TIMEOUT:
+                time.sleep(SEND_TIMEOUT - seconds_elapsed)
+            while last_send < new_send:
+                last_send = last_send + datetime.timedelta(seconds=10)
+            logging.info("data sent")
+
+
+if __name__ == '__main__':
+    batch_monitoring()
---
+++ training/re-train.py
@@ -0,0 +1,103 @@
+import re
+import string
+
+import mlflow
+import pandas as pd
+import unidecode
+import contractions
+from prefect import flow, task
+from utils.emoticons import EMOTICONS
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+@task(name="Load Data")
+def load_data(path):
+    df = pd.read_parquet(path)
+    return df
+
+
+@task(name="Clean Data")
+def clean_text(text):
+    # Convert the text to lowercase
+    text = text.lower()
+
+    # Remove HTML entities and special characters
+    text = re.sub(r'(&amp;|&lt;|&gt;|\n|\t)', '', text)
+
+    # Remove URLs
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+
+    # Remove email addresses
+    text = re.sub(r'\S+@\S+', '', text)
+
+    # Remove dates in various formats (e.g., DD-MM-YYYY, MM/DD/YY)
+    text = re.sub(r'\d{1,2}(st|nd|rd|th)?[-./]\d{1,2}[-./]\d{2,4}', '', text)
+
+    # Remove month-day-year patterns (e.g., Jan 1st, 2022)
+    pattern = re.compile(
+        r'(\d{1,2})?(st|nd|rd|th)?[-./,]?\s?(of)?\s?([J|j]an(uary)?|[F|f]eb(ruary)?|[Mm]ar(ch)?|[Aa]pr(il)?|[Mm]ay|[Jj]un(e)?|[Jj]ul(y)?|[Aa]ug(ust)?|[Ss]ep(tember)?|[Oo]ct(ober)?|[Nn]ov(ember)?|[Dd]ec(ember)?)\s?(\d{1,2})?(st|nd|rd|th)?\s?[-./,]?\s?(\d{2,4})?'
+    )
+    text = pattern.sub(r'', text)
+
+    # Remove emoticons
+    emoticons_pattern = re.compile(u'(' + u'|'.join(emo for emo in EMOTICONS) + u')')
+    text = emoticons_pattern.sub(r'', text)
+
+    # Remove mentions (@) and hashtags (#)
+    text = re.sub(r'(@\S+|#\S+)', '', text)
+
+    # Fix contractions (e.g., "I'm" becomes "I am")
+    text = contractions.fix(text)
+
+    # Remove punctuation
+    PUNCTUATIONS = string.punctuation
+    text = text.translate(str.maketrans('', '', PUNCTUATIONS))
+
+    # Remove unicode
+    text = unidecode.unidecode(text)
+
+    # Replace multiple whitespaces with a single space
+    text = re.sub(r'\s+', ' ', text)
+
+    return text
+
+
+@flow(name="Train Model")
+def start_training():
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment("Re-training Model")
+
+    # Load the data
+    df = load_data("../data/raw/train.parquet")
+
+    # Clean the text
+    df["processed_text"] = df["text"].apply(clean_text)
+
+    # Create Pipeline
+    pipeline = Pipeline(
+        [
+            (
+                'vectorizer',
+                TfidfVectorizer(
+                    stop_words='english', min_df=2, max_df=0.75, ngram_range=(1, 2)
+                ),
+            ),
+            ('clf', LogisticRegression(solver='liblinear', penalty='l2', C=1.0)),
+        ]
+    )
+
+    # Train the model
+    pipeline.fit(df["processed_text"], df["target"])
+
+    # Log the model
+    with mlflow.start_run():
+        mlflow.set_tag("model", "Logistic Regression")
+        mlflow.set_tag("tag", "Re-tarin")
+
+        mlflow.sklearn.log_model(pipeline, "model")
+
+
+if __name__ == "__main__":
+    start_training()
